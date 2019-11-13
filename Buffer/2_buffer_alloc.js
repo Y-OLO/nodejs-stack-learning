f8f1d6ad93f78098f80489338a85fb9c1eadddea
guofeng <136566468@>
2019-11-13 10:11:56 +0800

    提交buffer测试

---
+++ Buffer/2_buffer_alloc.js
@@ -0,0 +1,3 @@
+const bAlloc1 = Buffer.alloc(10); // 创建一个大小为 10 个字节的缓冲区
+
+console.log(bAlloc1); // <Buffer 00 00 00 00 00 00 00 00 00 00>
---
+++ Buffer/4_buffer_ 编码.js
@@ -0,0 +1,2 @@
+const buf = Buffer.from('hello world', 'ascii');
+console.log(buf.toString('hex')); // 68656c6c6f20776f726c64
