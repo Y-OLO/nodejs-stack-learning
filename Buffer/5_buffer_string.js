2d917070aa0f8f43438aa898f52ffbc7798b4e27
guofeng <136566468@>
2019-11-13 11:15:33 +0800

    buffer初始化

---
+++ Buffer/5_buffer_string.js
@@ -0,0 +1,4 @@
+const buf = Buffer.from('Node.js 技术栈 Buffer', 'UTF-8');
+
+console.log(buf); // <Buffer 4e 6f 64 65 2e 6a 73 20 e6 8a 80 e6 9c af e6 a0 88 20 42 75 66 66 65 72>
+console.log(buf.length); // 24
---
+++ Buffer/9_buffer_vs_string.js
@@ -0,0 +1,20 @@
+const http = require('http');
+let s = '';
+for (let i=0; i<1024*10; i++) {
+    s+='a'
+}
+
+const str = s;
+const bufStr = Buffer.from(s);
+const server = http.createServer((req, res) => {
+    process.title ='测试Buffer Vs String'
+    console.log(req.url);
+
+    if (req.url === '/buffer') {
+        res.end(bufStr);
+    } else if (req.url === '/string') {
+        res.end(str);
+    }
+});
+
+server.listen(3100);
