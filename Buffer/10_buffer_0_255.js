256899f507a542f9f7ca3e4bd8128678a3caf627
guofeng <136566468@>
2019-11-18 17:14:37 +0800

    添加buffer 限制0-255

---
+++ Buffer/10_buffer_0_255.js
@@ -0,0 +1,13 @@
+var buf = new Buffer(100);
+console.log(buf.length); // => 100
+console.log(buf[10]);
+
+//我们给buffer元素赋值
+buf[10] = 100;
+console.log(buf[10]); // => 100
+buf[20] = -100;
+console.log(buf[20]); // 156
+buf[21] = 300;
+console.log(buf[21]); // 44
+buf[22] = 3.1415;
+console.log(buf[22]); // 3
