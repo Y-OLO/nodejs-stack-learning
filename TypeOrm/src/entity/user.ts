729f031c3f2ed99f69b46f14412123831dbb1140
guofeng <136566468@>
2019-11-29 14:23:53 +0800

    删除无用引用

--- TypeOrm/src/entity/user.ts
+++ TypeOrm/src/entity/user.ts
@@ -1,34 +1,34 @@
-import {BaseEntity,Column,Entity,Index,JoinColumn,JoinTable,ManyToMany,ManyToOne,OneToMany,OneToOne,PrimaryColumn,PrimaryGeneratedColumn,RelationId} from "typeorm";
+import { BaseEntity, Column, Entity, Index, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn, PrimaryGeneratedColumn, RelationId } from "typeorm";
 
 
-@Entity("user" ,{schema:"test_typeorm" } )
+@Entity("user", { schema: "test_typeorm" })
 export class User {
 
-    @PrimaryGeneratedColumn({
-        type:"int", 
-        name:"id"
-        })
-    id:number;
-        
+  @PrimaryGeneratedColumn({
+    type: "int",
+    name: "id"
+  })
+  id: number;
 
-    @Column("int",{ 
-        nullable:false,
-        name:"age"
-        })
-    age:number;
-        
 
-    @Column("varchar",{ 
-        nullable:false,
-        name:"firstName"
-        })
-    firstName:string;
-        
+  @Column("int", {
+    nullable: false,
+    name: "age"
+  })
+  age: number;
 
-    @Column("varchar",{ 
-        nullable:false,
-        name:"lastName"
-        })
-    lastName:string;
-        
+
+  @Column("varchar", {
+    nullable: false,
+    name: "firstName"
+  })
+  firstName: string;
+
+
+  @Column("varchar", {
+    nullable: false,
+    name: "lastName"
+  })
+  lastName: string;
+
 }
